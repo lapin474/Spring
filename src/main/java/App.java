6695d908c32d585b02a36121efc364a51227d521
test java project <danilair@icloud.com>
2025-04-14 17:02:22 +0300

    finish 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,21 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean_1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean_1.getMessage());
+
+        HelloWorld bean_2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean_2.getMessage());
+
+        System.out.println("Одинаковые ли объекты 'HelloWorld' ? -> " + (bean_1 == bean_2));
+
+        Cat bean_3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean_3.getMessage());
+
+        Cat bean_4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean_4.getMessage());
+
+        System.out.println("Одинаковые ли объекты 'Cat' ? -> " + (bean_3 == bean_4));
     }
 }
